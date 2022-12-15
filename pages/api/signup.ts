9e43d79158671d84af708cb6e7d36393537f0a96
netlemon <netlemon2021@gmail.com>
2022-12-15 21:14:54 +0200

    added naive singup implementation

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -5,4 +5,41 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import prisma from '../../lib/prisma'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync()
+  const { email, password } = req.body
+
+  let user
+
+  try {
+    user = await prisma.user.create({
+      data: { email, password: bcrypt.hashSync(password, salt) },
+    })
+  } catch (error) {
+    res.status(401)
+    res.json({ error: 'User already exists' })
+    return
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    'secret',
+    { expiresIn: '8h' }
+  )
+
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('TRAX_ACCESS_TOKEN', token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+    })
+  )
+  res.json(user)
+}

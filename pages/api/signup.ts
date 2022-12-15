749ee54cd5a933000c21a9a3a07f4632d116b3e3
netlemon <netlemon2021@gmail.com>
2022-12-15 21:00:01 +0200

    Added prisma as a singleton, and started template for user sign up

---
+++ pages/api/signup.ts
@@ -0,0 +1,8 @@
+import cookie from 'cookie'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import prisma from '../../lib/prisma'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
 
 import { artistsData } from './songsData'

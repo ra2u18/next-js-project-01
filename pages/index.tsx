3bec9ca17f24e159cd1da1be81376815caad6eb0
netlemon <netlemon2021@gmail.com>
2022-12-17 18:01:37 +0200

    homepage ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-const Home = () => <div>Home</div>
+import GradientLayout from '../components/gradientLayout'
+
+const Home = () => (
+  <GradientLayout
+    color="red"
+    subtitle="profile"
+    title="Rick Andronache"
+    description="15 public playlists"
+    image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+    roundImage
+  >
+    <div>Homepage</div>
+  </GradientLayout>
+)
 
 export default Home
